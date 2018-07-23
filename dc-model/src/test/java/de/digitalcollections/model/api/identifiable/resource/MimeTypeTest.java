0c46823af01b8276971b36666deeddf908f49b85
Stefan Schweter <stefan.schweter@bsb-muenchen.de>
2018-07-23 18:02:32 +0200

    dc-model: update unit tests to JUnit 5

--- dc-model/src/test/java/de/digitalcollections/model/api/identifiable/resource/MimeTypeTest.java
+++ dc-model/src/test/java/de/digitalcollections/model/api/identifiable/resource/MimeTypeTest.java
@@ -1,7 +1,7 @@
 package de.digitalcollections.model.api.identifiable.resource;
 
 import java.net.URI;
-import org.junit.Test;
+import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
