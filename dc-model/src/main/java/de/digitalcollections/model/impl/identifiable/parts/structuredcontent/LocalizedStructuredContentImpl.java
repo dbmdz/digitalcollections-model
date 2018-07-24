bc98fc53f3f4ed7765b8acf6e732edea7eaa23d1
Matthias Lindinger <matthias.lindinger@bsb-muenchen.de>
2018-07-24 09:39:09 +0200

    Add constructor with empty content

--- dc-model/src/main/java/de/digitalcollections/model/impl/identifiable/parts/structuredcontent/LocalizedStructuredContentImpl.java
+++ dc-model/src/main/java/de/digitalcollections/model/impl/identifiable/parts/structuredcontent/LocalizedStructuredContentImpl.java
@@ -2,12 +2,25 @@ package de.digitalcollections.model.impl.identifiable.parts.structuredcontent;
 
 import de.digitalcollections.model.api.identifiable.parts.structuredcontent.LocalizedStructuredContent;
 import de.digitalcollections.model.api.identifiable.parts.structuredcontent.StructuredContent;
+import de.digitalcollections.model.impl.identifiable.parts.structuredcontent.contentblocks.ParagraphImpl;
 import java.util.Locale;
 import java.util.Map;
 
 public class LocalizedStructuredContentImpl implements LocalizedStructuredContent {
 
   private Map<Locale, StructuredContent> localizedStructuredContent;
+
+  public LocalizedStructuredContentImpl() {
+  }
+
+  // FIXME: use service to get empty document or move logic to javascript
+  @Deprecated
+  public LocalizedStructuredContentImpl(Locale locale) {
+    this();
+    StructuredContent document = new StructuredContentImpl();
+    document.addContentBlock(new ParagraphImpl());
+    add(locale, document);
+  }
 
   @Override
   public Map<Locale, StructuredContent> getLocalizedStructuredContent() {
