d6d0a735ec5a0fb4407cc4b9acadce6343eaeb5b
Ralf Eichinger <ralf.eichinger@bsb-muenchen.de>
2018-11-30 08:41:36 +0100

    Add ResourceNotFoundException to model

---
+++ dc-model/src/main/java/de/digitalcollections/model/api/identifiable/resource/exceptions/ResourceNotFoundException.java
@@ -0,0 +1,19 @@
+package de.digitalcollections.model.api.identifiable.resource.exceptions;
+
+public class ResourceNotFoundException extends Exception {
+
+  public ResourceNotFoundException() {
+  }
+
+  public ResourceNotFoundException(String message) {
+    super(message);
+  }
+
+  public ResourceNotFoundException(String message, Throwable cause) {
+    super(message, cause);
+  }
+
+  public ResourceNotFoundException(Throwable cause) {
+    super(cause);
+  }
+}
