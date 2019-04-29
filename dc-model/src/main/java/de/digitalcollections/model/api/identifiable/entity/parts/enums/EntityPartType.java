9be236ceec855de44723b5191359d6e240c6c186
Ralf Eichinger <ralf.eichinger@bsb-muenchen.de>
2019-04-29 15:19:57 +0200

    Remove IdentifiablesContainer and introduce EntityPart(Impl) classes

---
+++ dc-model/src/main/java/de/digitalcollections/model/api/identifiable/entity/parts/enums/EntityPartType.java
@@ -0,0 +1,15 @@
+package de.digitalcollections.model.api.identifiable.entity.parts.enums;
+
+import de.digitalcollections.model.api.identifiable.entity.parts.EntityPart;
+
+/**
+ * All {@link EntityPart} types cudami can handle
+ */
+public enum EntityPartType {
+  CONTENT_NODE, WEBPAGE;
+
+  @Override
+  public String toString() {
+    return name();
+  }
+}
--- dc-model/src/main/java/de/digitalcollections/model/impl/identifiable/entity/ArticleImpl.java
+++ dc-model/src/main/java/de/digitalcollections/model/impl/identifiable/entity/ArticleImpl.java
@@ -1,32 +1,16 @@
 package de.digitalcollections.model.impl.identifiable.entity;
 
-import de.digitalcollections.model.api.identifiable.Identifiable;
-import de.digitalcollections.model.api.identifiable.IdentifiablesContainer;
 import de.digitalcollections.model.api.identifiable.entity.Article;
 import de.digitalcollections.model.api.identifiable.entity.enums.EntityType;
 import de.digitalcollections.model.api.identifiable.parts.structuredcontent.LocalizedStructuredContent;
-import de.digitalcollections.model.impl.identifiable.IdentifiablesContainerImpl;
-import java.util.List;
 
 public class ArticleImpl extends EntityImpl implements Article<Article> {
 
-  private final IdentifiablesContainer identifiablesContainer;
   private LocalizedStructuredContent text;
 
   public ArticleImpl() {
     super();
     this.entityType = EntityType.ARTICLE;
-    this.identifiablesContainer = new IdentifiablesContainerImpl();
-  }
-
-  @Override
-  public List<Identifiable> getIdentifiables() {
-    return identifiablesContainer.getIdentifiables();
-  }
-
-  @Override
-  public void setIdentifiables(List<Identifiable> identifiables) {
-    identifiablesContainer.setIdentifiables(identifiables);
   }
 
   @Override
