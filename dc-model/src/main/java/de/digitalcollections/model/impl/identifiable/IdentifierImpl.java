3d7ce323719c69d63e6137e97c21f9963e41613a
Ralf Eichinger <ralf.eichinger@bsb-muenchen.de>
2019-05-20 10:47:46 +0200

    Add identifiable uuid to identifier to express related object (identifiable)

--- dc-model/src/main/java/de/digitalcollections/model/impl/identifiable/IdentifierImpl.java
+++ dc-model/src/main/java/de/digitalcollections/model/impl/identifiable/IdentifierImpl.java
@@ -2,18 +2,22 @@ package de.digitalcollections.model.impl.identifiable;
 
 import de.digitalcollections.model.api.identifiable.Identifier;
 import java.io.Serializable;
+import java.util.UUID;
 
 public class IdentifierImpl implements Identifier, Serializable {
 
   private String id;
+
+  private UUID identifiableUuid;
 
   private String namespace;
 
   public IdentifierImpl() {
   }
 
-  public IdentifierImpl(String namespace, String id) {
+  public IdentifierImpl(UUID identifiableUuid, String namespace, String id) {
     this.id = id;
+    this.identifiableUuid = identifiableUuid;
     this.namespace = namespace;
   }
 
@@ -28,6 +32,16 @@ public class IdentifierImpl implements Identifier, Serializable {
   }
 
   @Override
+  public UUID getIdentifiableUuid() {
+    return this.identifiableUuid;
+  }
+
+  @Override
+  public void setIdentifiableUuid(UUID identifiableUuid) {
+    this.identifiableUuid = identifiableUuid;
+  }
+
+  @Override
   public String getNamespace() {
     return namespace;
   }
@@ -39,6 +53,6 @@ public class IdentifierImpl implements Identifier, Serializable {
 
   @Override
   public String toString() {
-    return namespace + ":" + id;
+    return namespace + ":" + id + ":" + identifiableUuid;
   }
 }
