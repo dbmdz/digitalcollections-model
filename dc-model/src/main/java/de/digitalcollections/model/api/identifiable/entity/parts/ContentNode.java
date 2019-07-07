bb9b21412ee0a0e436b200f997301384607908dc
Ralf Eichinger <ralf.eichinger@bsb-muenchen.de>
2019-07-07 20:54:43 +0200

    Add entities and fileresources to content node

--- dc-model/src/main/java/de/digitalcollections/model/api/identifiable/entity/parts/ContentNode.java
+++ dc-model/src/main/java/de/digitalcollections/model/api/identifiable/entity/parts/ContentNode.java
@@ -1,6 +1,33 @@
 package de.digitalcollections.model.api.identifiable.entity.parts;
 
 import de.digitalcollections.model.api.identifiable.Node;
+import de.digitalcollections.model.api.identifiable.entity.Entity;
+import de.digitalcollections.model.api.identifiable.resource.FileResource;
+import java.util.LinkedHashSet;
 
 public interface ContentNode<N extends Node> extends Node<N>, EntityPart {
+
+  LinkedHashSet<Entity> getEntities();
+
+  void setEntities(LinkedHashSet<Entity> entities);
+
+  default LinkedHashSet<Entity> addEntity(Entity entity) {
+    if (getEntities() == null) {
+      setEntities(new LinkedHashSet<>());
+    }
+    getEntities().add(entity);
+    return getEntities();
+  }
+
+  LinkedHashSet<FileResource> getFileResources();
+
+  void setFileResources(LinkedHashSet<FileResource> fileResources);
+
+  default LinkedHashSet<FileResource> addFileResource(FileResource fileResource) {
+    if (getFileResources() == null) {
+      setFileResources(new LinkedHashSet<>());
+    }
+    getFileResources().add(fileResource);
+    return getFileResources();
+  }
 }
