e78acf7a034ccf7434c194c2501f20d54e0f0f69
Ralf Eichinger <ralf.eichinger@bsb-muenchen.de>
2019-07-07 20:54:43 +0200

    Add entities and fileresources to content node

--- dc-model/src/main/java/de/digitalcollections/model/impl/identifiable/entity/parts/ContentNodeImpl.java
+++ dc-model/src/main/java/de/digitalcollections/model/impl/identifiable/entity/parts/ContentNodeImpl.java
@@ -1,12 +1,18 @@
 package de.digitalcollections.model.impl.identifiable.entity.parts;
 
+import de.digitalcollections.model.api.identifiable.entity.Entity;
 import de.digitalcollections.model.api.identifiable.entity.parts.ContentNode;
 import de.digitalcollections.model.api.identifiable.entity.parts.enums.EntityPartType;
+import de.digitalcollections.model.api.identifiable.resource.FileResource;
 import de.digitalcollections.model.impl.identifiable.NodeImpl;
+import java.util.LinkedHashSet;
 import java.util.List;
 
 public class ContentNodeImpl extends EntityPartImpl implements ContentNode<ContentNode> {
+
   private final NodeImpl<ContentNode> node;
+  private LinkedHashSet<Entity> entites;
+  private LinkedHashSet<FileResource> fileResources;
 
   public ContentNodeImpl() {
     super();
@@ -32,5 +38,25 @@ public class ContentNodeImpl extends EntityPartImpl implements ContentNode<Conte
   @Override
   public void setParent(ContentNode parent) {
     node.setParent(parent);
+  }
+
+  @Override
+  public LinkedHashSet<Entity> getEntities() {
+    return entites;
+  }
+
+  @Override
+  public void setEntities(LinkedHashSet<Entity> entities) {
+    this.entites = entities;
+  }
+
+  @Override
+  public LinkedHashSet<FileResource> getFileResources() {
+    return fileResources;
+  }
+
+  @Override
+  public void setFileResources(LinkedHashSet<FileResource> fileResources) {
+    this.fileResources = fileResources;
   }
 }
