d40bdf5a0a8d939b7c2a870ce0b9269612ff58ba
Matthias Lindinger <matthias.lindinger@bsb-muenchen.de>
2019-07-17 10:14:07 +0200

    Add unit test for whole webpage object

--- dc-model-jackson/src/test/java/de/digitalcollections/model/jackson/identifiable/entity/parts/WebpageJsonTest.java
+++ dc-model-jackson/src/test/java/de/digitalcollections/model/jackson/identifiable/entity/parts/WebpageJsonTest.java
@@ -1,14 +1,41 @@
 package de.digitalcollections.model.jackson.identifiable.entity.parts;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import de.digitalcollections.model.api.identifiable.entity.parts.Webpage;
 import de.digitalcollections.model.jackson.BaseJsonSerializationTest;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 public class WebpageJsonTest extends BaseJsonSerializationTest {
 
+  private String expectedJson;
+  private static Webpage webpage;
+  private ObjectMapper mapper;
+
+  @BeforeAll
+  public static void setUpGlobal() {
+    webpage = WebpageFactory.create();
+  }
+
+  @BeforeEach
+  public void setUp() throws Exception {
+    expectedJson = readFromResources("serializedTestObjects/webpage.json").replaceAll("\\s", "");
+    mapper = getMapper();
+  }
+
+  @Test
+  public void testSerialization() throws Exception {
+    webpage.setCreated(null);
+    webpage.setLastModified(null);
+    String json = mapper.writerWithDefaultPrettyPrinter().writeValueAsString(webpage).replaceAll("\\s", "");
+    assertThat(json).isEqualTo(expectedJson);
+  }
+
   @Test
   public void testSerialisationInBothWays() throws Exception {
-    Webpage webpage = WebpageFactory.create();
     checkSerializeDeserialize(webpage);
   }
 }
