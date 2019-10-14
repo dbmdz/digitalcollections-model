1f7615560fc22b4cf2076784153fea8f04c4599f
Ralf Eichinger <ralf.eichinger@bsb-muenchen.de>
2019-10-14 11:20:00 +0200

    Introducing model corporation and project

--- dc-model-jackson/src/test/java/de/digitalcollections/model/jackson/identifiable/entity/ProjectTest.java
+++ dc-model-jackson/src/test/java/de/digitalcollections/model/jackson/identifiable/entity/ProjectTest.java
@@ -4,6 +4,7 @@ import de.digitalcollections.model.api.identifiable.entity.Project;
 import de.digitalcollections.model.impl.identifiable.entity.ProjectImpl;
 import de.digitalcollections.model.impl.identifiable.parts.LocalizedTextImpl;
 import de.digitalcollections.model.jackson.BaseJsonSerializationTest;
+import java.time.LocalDate;
 import java.util.Locale;
 import org.junit.jupiter.api.Test;
 
@@ -13,6 +14,8 @@ public class ProjectTest extends BaseJsonSerializationTest {
   public void testSerialisationInBothWays() throws Exception {
     Project project = new ProjectImpl();
     project.setLabel(new LocalizedTextImpl(Locale.GERMAN, "Projekt XY"));
+    project.setStartDate(LocalDate.of(2017, 06, 16));
+    project.setStartDate(LocalDate.of(2019, 07, 31));
     checkSerializeDeserialize(project);
   }
 }
