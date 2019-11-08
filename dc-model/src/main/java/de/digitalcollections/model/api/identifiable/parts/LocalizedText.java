0b8461d5e81329162eb56a2f0f3b8ca76a27dd6b
Matthias Lindinger <matthias.lindinger@bsb-muenchen.de>
2019-11-08 14:12:03 +0100

    Fix return type

--- dc-model/src/main/java/de/digitalcollections/model/api/identifiable/parts/LocalizedText.java
+++ dc-model/src/main/java/de/digitalcollections/model/api/identifiable/parts/LocalizedText.java
@@ -1,6 +1,6 @@
 package de.digitalcollections.model.api.identifiable.parts;
 
-import java.util.Collection;
+import java.util.ArrayList;
 import java.util.Locale;
 import java.util.Map;
 
@@ -8,7 +8,7 @@ import java.util.Map;
 public interface LocalizedText extends Map<Locale, String> {
 
   /** @return all locales for which translated texts are available. */
-  Collection<Locale> getLocales();
+  ArrayList<Locale> getLocales();
 
   /** @return first found text */
   String getText();
--- dc-model/src/main/java/de/digitalcollections/model/impl/identifiable/parts/LocalizedTextImpl.java
+++ dc-model/src/main/java/de/digitalcollections/model/impl/identifiable/parts/LocalizedTextImpl.java
@@ -1,9 +1,8 @@
 package de.digitalcollections.model.impl.identifiable.parts;
 
 import de.digitalcollections.model.api.identifiable.parts.LocalizedText;
-import java.util.Collection;
+import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.LinkedHashSet;
 import java.util.Locale;
 
 public class LocalizedTextImpl extends HashMap<Locale, String> implements LocalizedText {
@@ -18,13 +17,13 @@ public class LocalizedTextImpl extends HashMap<Locale, String> implements Locali
   }
 
   @Override
-  public Collection<Locale> getLocales() {
-    return new LinkedHashSet<>(this.keySet());
+  public ArrayList<Locale> getLocales() {
+    return new ArrayList<>(this.keySet());
   }
 
   @Override
   public String getText() {
-    Collection<Locale> locales = getLocales();
+    ArrayList<Locale> locales = getLocales();
     if (locales.isEmpty()) {
       return null;
     }
