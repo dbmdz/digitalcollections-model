7bfd7b2fa358af61757fa39ced1e719c131b5177
Matthias Lindinger <matthias.lindinger@bsb-muenchen.de>
2020-03-02 11:51:52 +0100

    Add iiif base url to ImageFileResource

--- dc-model/src/main/java/de/digitalcollections/model/api/identifiable/resource/ImageFileResource.java
+++ dc-model/src/main/java/de/digitalcollections/model/api/identifiable/resource/ImageFileResource.java
@@ -1,16 +1,24 @@
 package de.digitalcollections.model.api.identifiable.resource;
 
+import java.net.URL;
+
 /** An image file resource. Mimetype starts with "image/". */
 public interface ImageFileResource extends FileResource {
 
   /** @return width in pixel */
   int getWidth();
 
+  /** @return the iiif base url */
+  URL getIiifBaseUrl();
+
   /** @return height in pixel */
   int getHeight();
 
   /** @param width width in pixel */
   void setWidth(int width);
+
+  /** @param iiifBaseUrl the iiif base url */
+  void setIiifBaseUrl(URL iiifBaseUrl);
 
   /** @param height height in pixel */
   void setHeight(int height);
--- dc-model/src/main/java/de/digitalcollections/model/impl/identifiable/resource/ImageFileResourceImpl.java
+++ dc-model/src/main/java/de/digitalcollections/model/impl/identifiable/resource/ImageFileResourceImpl.java
@@ -1,15 +1,22 @@
 package de.digitalcollections.model.impl.identifiable.resource;
 
 import de.digitalcollections.model.api.identifiable.resource.ImageFileResource;
+import java.net.URL;
 
 public class ImageFileResourceImpl extends FileResourceImpl implements ImageFileResource {
 
   private int height;
+  private URL iiifBaseUrl;
   private int width;
 
   @Override
   public int getWidth() {
     return width;
+  }
+
+  @Override
+  public URL getIiifBaseUrl() {
+    return iiifBaseUrl;
   }
 
   @Override
@@ -20,6 +27,11 @@ public class ImageFileResourceImpl extends FileResourceImpl implements ImageFile
   @Override
   public void setWidth(int width) {
     this.width = width;
+  }
+
+  @Override
+  public void setIiifBaseUrl(URL iiifBaseUrl) {
+    this.iiifBaseUrl = iiifBaseUrl;
   }
 
   @Override
