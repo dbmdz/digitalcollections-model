a30ff1d95dd16061616ef6a293b2429e0b26caf4
Ralf Eichinger <ralf.eichinger@bsb-muenchen.de>
2020-03-21 14:21:21 +0100

    Add generic fileresource type (undefined)

--- dc-model-jackson/src/main/java/de/digitalcollections/model/jackson/mixin/identifiable/resource/FileResourceMixIn.java
+++ dc-model-jackson/src/main/java/de/digitalcollections/model/jackson/mixin/identifiable/resource/FileResourceMixIn.java
@@ -5,6 +5,7 @@ import com.fasterxml.jackson.annotation.JsonSubTypes;
 import com.fasterxml.jackson.annotation.JsonTypeInfo;
 import de.digitalcollections.model.impl.identifiable.resource.ApplicationFileResourceImpl;
 import de.digitalcollections.model.impl.identifiable.resource.AudioFileResourceImpl;
+import de.digitalcollections.model.impl.identifiable.resource.FileResourceImpl;
 import de.digitalcollections.model.impl.identifiable.resource.ImageFileResourceImpl;
 import de.digitalcollections.model.impl.identifiable.resource.LinkedDataFileResourceImpl;
 import de.digitalcollections.model.impl.identifiable.resource.TextFileResourceImpl;
@@ -19,6 +20,7 @@ import de.digitalcollections.model.impl.identifiable.resource.VideoFileResourceI
 @JsonSubTypes({
   @JsonSubTypes.Type(value = ApplicationFileResourceImpl.class, name = "APPLICATION"),
   @JsonSubTypes.Type(value = AudioFileResourceImpl.class, name = "AUDIO"),
+  @JsonSubTypes.Type(value = FileResourceImpl.class, name = "UNDEFINED"),
   @JsonSubTypes.Type(value = ImageFileResourceImpl.class, name = "IMAGE"),
   @JsonSubTypes.Type(value = LinkedDataFileResourceImpl.class, name = "LINKED_DATA"),
   @JsonSubTypes.Type(value = TextFileResourceImpl.class, name = "TEXT"),
--- dc-model/src/main/java/de/digitalcollections/model/api/identifiable/resource/enums/FileResourceType.java
+++ dc-model/src/main/java/de/digitalcollections/model/api/identifiable/resource/enums/FileResourceType.java
@@ -6,6 +6,7 @@ public enum FileResourceType {
   IMAGE,
   LINKED_DATA,
   TEXT,
+  UNDEFINED,
   VIDEO;
 
   @Override
