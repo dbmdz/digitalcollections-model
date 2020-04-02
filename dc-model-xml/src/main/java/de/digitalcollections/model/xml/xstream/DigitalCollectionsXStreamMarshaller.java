8c0af4edfbc66e1ee26a753c456ffa12ece39af3
Matthias Lindinger <matthias.lindinger@bsb-muenchen.de>
2020-04-02 17:08:35 +0200

    Add more xml aliases for classes

--- dc-model-xml/src/main/java/de/digitalcollections/model/xml/xstream/DigitalCollectionsXStreamMarshaller.java
+++ dc-model-xml/src/main/java/de/digitalcollections/model/xml/xstream/DigitalCollectionsXStreamMarshaller.java
@@ -2,6 +2,13 @@ package de.digitalcollections.model.xml.xstream;
 
 import com.thoughtworks.xstream.XStream;
 import de.digitalcollections.model.api.identifiable.parts.structuredcontent.ContentBlock;
+import de.digitalcollections.model.impl.identifiable.entity.ArticleImpl;
+import de.digitalcollections.model.impl.identifiable.entity.CollectionImpl;
+import de.digitalcollections.model.impl.identifiable.entity.ContentTreeImpl;
+import de.digitalcollections.model.impl.identifiable.entity.CorporationImpl;
+import de.digitalcollections.model.impl.identifiable.entity.ProjectImpl;
+import de.digitalcollections.model.impl.identifiable.entity.WebsiteImpl;
+import de.digitalcollections.model.impl.identifiable.entity.parts.ContentNodeImpl;
 import de.digitalcollections.model.impl.identifiable.entity.parts.WebpageImpl;
 import de.digitalcollections.model.impl.identifiable.parts.TranslationImpl;
 import de.digitalcollections.model.impl.identifiable.parts.structuredcontent.StructuredContentImpl;
@@ -21,7 +28,13 @@ import de.digitalcollections.model.impl.identifiable.parts.structuredcontent.con
 import de.digitalcollections.model.impl.identifiable.parts.structuredcontent.contentblocks.TableImpl;
 import de.digitalcollections.model.impl.identifiable.parts.structuredcontent.contentblocks.TableRowImpl;
 import de.digitalcollections.model.impl.identifiable.parts.structuredcontent.contentblocks.TextImpl;
+import de.digitalcollections.model.impl.identifiable.resource.ApplicationFileResourceImpl;
+import de.digitalcollections.model.impl.identifiable.resource.AudioFileResourceImpl;
 import de.digitalcollections.model.impl.identifiable.resource.FileResourceImpl;
+import de.digitalcollections.model.impl.identifiable.resource.ImageFileResourceImpl;
+import de.digitalcollections.model.impl.identifiable.resource.LinkedDataFileResourceImpl;
+import de.digitalcollections.model.impl.identifiable.resource.TextFileResourceImpl;
+import de.digitalcollections.model.impl.identifiable.resource.VideoFileResourceImpl;
 import java.util.HashMap;
 import java.util.Map;
 import org.springframework.oxm.xstream.XStreamMarshaller;
@@ -57,27 +70,40 @@ public class DigitalCollectionsXStreamMarshaller extends XStreamMarshaller {
 
   private Map<String, Class> getAliases() {
     Map<String, Class> aliases = new HashMap<>();
+    aliases.put("applicationFileresource", ApplicationFileResourceImpl.class);
+    aliases.put("article", ArticleImpl.class);
+    aliases.put("audioFileresource", AudioFileResourceImpl.class);
     aliases.put("blockquote", BlockquoteImpl.class);
     aliases.put("bulletList", BulletListImpl.class);
     aliases.put("codeBlock", CodeBlockImpl.class);
+    aliases.put("collection", CollectionImpl.class);
     aliases.put("contentBlocks", ContentBlock.class);
+    aliases.put("contentNode", ContentNodeImpl.class);
+    aliases.put("contentTree", ContentTreeImpl.class);
+    aliases.put("corporation", CorporationImpl.class);
     aliases.put("fileresource", FileResourceImpl.class);
     aliases.put("hardBreak", HardBreakImpl.class);
     aliases.put("heading", HeadingImpl.class);
     aliases.put("horizontalRule", HorizontalRuleImpl.class);
     aliases.put("iframe", IFrameImpl.class);
     aliases.put("image", ImageImpl.class);
+    aliases.put("imageFileresource", ImageFileResourceImpl.class);
+    aliases.put("linkedDataFileresource", LinkedDataFileResourceImpl.class);
     aliases.put("listItem", ListItemImpl.class);
     aliases.put("mark", MarkImpl.class);
     aliases.put("orderedList", OrderedListImpl.class);
     aliases.put("paragraph", ParagraphImpl.class);
+    aliases.put("project", ProjectImpl.class);
     aliases.put("structuredContent", StructuredContentImpl.class);
     aliases.put("table", TableImpl.class);
     aliases.put("tableCell", TableCellImpl.class);
     aliases.put("tableRow", TableRowImpl.class);
     aliases.put("text", TextImpl.class);
+    aliases.put("textFileresource", TextFileResourceImpl.class);
     aliases.put("translation", TranslationImpl.class);
+    aliases.put("videoFileresource", VideoFileResourceImpl.class);
     aliases.put("webpage", WebpageImpl.class);
+    aliases.put("website", WebsiteImpl.class);
     return aliases;
   }
 }
