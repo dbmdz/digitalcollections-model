d35a3b6936f271389ac835daf7fb8086369d0e7e
Christoph Lorenz <clorenz71@googlemail.com>
2020-06-17 13:50:35 +0200

    Renaming of the navigation items

--- dc-model/src/main/java/de/digitalcollections/model/impl/view/BreadcrumbNavigationImpl.java
+++ dc-model/src/main/java/de/digitalcollections/model/impl/view/BreadcrumbNavigationImpl.java
@@ -2,22 +2,21 @@ package de.digitalcollections.model.impl.view;
 
 import de.digitalcollections.model.api.identifiable.Node;
 import de.digitalcollections.model.api.view.BreadcrumbNavigation;
-import de.digitalcollections.model.impl.identifiable.NodeImpl;
 import java.util.ArrayList;
 import java.util.List;
 
 public class BreadcrumbNavigationImpl implements BreadcrumbNavigation {
 
-  private List<Node> locationItems = new ArrayList<>();
+  private List<Node> navigationItems = new ArrayList<>();
 
   public BreadcrumbNavigationImpl() {}
 
-  public BreadcrumbNavigationImpl(List<Node> locationItems) {
-    this.locationItems = locationItems;
+  public BreadcrumbNavigationImpl(List<Node> navigationItems) {
+    this.navigationItems = navigationItems;
   }
 
   @Override
-  public List<Node> getLocationItems() {
-    return locationItems;
+  public List<Node> getNavigationItems() {
+    return navigationItems;
   }
 }
