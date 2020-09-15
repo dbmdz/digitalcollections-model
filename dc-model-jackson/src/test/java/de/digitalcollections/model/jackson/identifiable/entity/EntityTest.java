5c2c19442cac799ee6a706b2b89458a8506b3077
Christoph Lorenz <christoph.lorenz@bsb-muenchen.de>
2020-09-15 17:37:18 +0200

    Add projects to the entity mixinm too

--- dc-model-jackson/src/test/java/de/digitalcollections/model/jackson/identifiable/entity/EntityTest.java
+++ dc-model-jackson/src/test/java/de/digitalcollections/model/jackson/identifiable/entity/EntityTest.java
@@ -2,6 +2,7 @@ package de.digitalcollections.model.jackson.identifiable.entity;
 
 import de.digitalcollections.model.api.identifiable.entity.Entity;
 import de.digitalcollections.model.impl.identifiable.entity.CorporationImpl;
+import de.digitalcollections.model.impl.identifiable.entity.ProjectImpl;
 import de.digitalcollections.model.impl.identifiable.parts.LocalizedTextImpl;
 import de.digitalcollections.model.jackson.BaseJsonSerializationTest;
 import java.util.Locale;
@@ -17,5 +18,13 @@ public class EntityTest extends BaseJsonSerializationTest {
     Entity corporation = new CorporationImpl();
     corporation.setLabel(new LocalizedTextImpl(Locale.GERMAN, "Bayerische Staatsbibliothek"));
     checkSerializeDeserialize(corporation);
+  }
+
+  @Test
+  @DisplayName("works with a project as entity")
+  public void testSerialisationForProjectsInBothWays() throws Exception {
+    Entity project = new ProjectImpl();
+    project.setLabel(new LocalizedTextImpl(Locale.GERMAN, "DAS Projekt"));
+    checkSerializeDeserialize(project);
   }
 }
