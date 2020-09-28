21c9339d1f1465f5fffc6ec854c9591151db09ff
Matthias Lindinger <matthias.lindinger@bsb-muenchen.de>
2020-09-28 08:26:00 +0200

    Add sorted methods

--- dc-model/src/main/java/de/digitalcollections/model/api/identifiable/entity/Collection.java
+++ dc-model/src/main/java/de/digitalcollections/model/api/identifiable/entity/Collection.java
@@ -8,18 +8,18 @@ import java.util.List;
 /** A collection of {@link Entity}s. */
 public interface Collection extends Entity, Node<Collection> {
 
-  LocalizedStructuredContent getText();
-
-  void setText(LocalizedStructuredContent text);
-
-  List<Entity> getEntities();
-
-  void setEntities(List<Entity> entities);
-
   default void addEntity(Entity entity) {
     if (getEntities() == null) {
       setEntities(new ArrayList<>());
     }
     getEntities().add(entity);
   }
+
+  List<Entity> getEntities();
+
+  void setEntities(List<Entity> entities);
+
+  LocalizedStructuredContent getText();
+
+  void setText(LocalizedStructuredContent text);
 }
