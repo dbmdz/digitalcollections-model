b78fa254608a792770aceb1c3732cf75db6703f3
Ralf Eichinger <ralf.eichinger@bsb-muenchen.de>
2020-10-06 11:19:35 +0200

    Add "not set" ad additional constraint

--- dc-model/src/main/java/de/digitalcollections/model/api/filter/enums/FilterOperation.java
+++ dc-model/src/main/java/de/digitalcollections/model/api/filter/enums/FilterOperation.java
@@ -5,19 +5,24 @@ package de.digitalcollections.model.api.filter.enums;
  *
  * <table border="1">
  * <caption>Mapping operation abbreviation to filter operation</caption>
- * <tr><th>Symbol</th><th>Operation                 </th><th>Example filter query param                                      </th></tr>
- * <tr><td>eq    </td><td> Equals                   </td><td>city=eq:Munich / publicationDate=eq:2020-01-01                  </td></tr>
- * <tr><td>neq   </td><td> Not Equals               </td><td>country=neq:de / publicationDate=neq:2020-01-01                 </td></tr>
- * <tr><td>gt    </td><td> Greater Than             </td><td>amount=gt:10000 / publicationDate=gt:2020-01-01                 </td></tr>
- * <tr><td>gte   </td><td> Greater Than or equals to</td><td>amount=gte:10000 / publicationDate=gte:2020-01-01               </td></tr>
- * <tr><td>lt    </td><td> Less Than                </td><td>amount=lt:10000 / publicationDate=lt:2020-01-01                 </td></tr>
- * <tr><td>lte   </td><td> Less Than or equals to   </td><td>amount=lte:10000 / publicationDate=lte:2020-01-01               </td></tr>
- * <tr><td>in    </td><td> In                       </td><td>country=in:uk,usa,au / publicationDate=in:2020-01-01,2020-12-24 </td></tr>
- * <tr><td>nin   </td><td> Not in                   </td><td>country=nin:fr,de,nz / publicationDate=nin:2020-01-01,2020-12-24</td></tr>
- * <tr><td>btn   </td><td> Between (inclusive)      </td><td>joiningDate=btn:2018-01-01,2016-01-01                           </td></tr>
- * <tr><td>like  </td><td> Like                     </td><td>firstName=like:John                                             </td></tr>
- * <tr><td>set   </td><td> value exists (not null)  </td><td>firstName=set:                                                  </td></tr>
- * <tr><td>notset</td><td> value is not set (null)  </td><td>firstName=notset:                                               </td></tr>
+ * <tr><th>Symbol    </th><th>Operation                        </th><th>Example filter query param               </th></tr>
+ * <tr><td>eq        </td><td>equals                           </td><td>city=eq:Munich	                         </td></tr>
+ * <tr><td>eq_notset </td><td>equals or not set                </td><td>city=eq_notset:Munich                    </td></tr>
+ * <tr><td>neq       </td><td>not equals                       </td><td>country=neq:de                           </td></tr>
+ * <tr><td>gt        </td><td>greater than                     </td><td>amount=gt:10000                          </td></tr>
+ * <tr><td>gt_notset </td><td>greater than or not set          </td><td>presentationEnd=gt_notset:2020-10-06     </td></tr>
+ * <tr><td>gte       </td><td>greater than or equals           </td><td>amount=gte:10000                         </td></tr>
+ * <tr><td>gte_notset</td><td>greater than or equals or not set</td><td>amount=gte_notset:10000                  </td></tr>
+ * <tr><td>lt        </td><td>less than                        </td><td>amount=lt:10000                          </td></tr>
+ * <tr><td>lt_notset </td><td>less than or not set             </td><td>amount=lt_notset:10000                   </td></tr>
+ * <tr><td>lte       </td><td>less than or equals to           </td><td>amount=lte:10000                         </td></tr>
+ * <tr><td>lte_notset</td><td>less than or equals or not set   </td><td>presentationStart=lte_notset:2020-10-06  </td></tr>
+ * <tr><td>in        </td><td>in                               </td><td>country=in:uk, usa, au                   </td></tr>
+ * <tr><td>nin       </td><td>not in                           </td><td>country=nin:fr, de, nz                   </td></tr>
+ * <tr><td>btn       </td><td>between (inclusive)              </td><td>joiningDate=btn:2018-01-01, 2016-01-01   </td></tr>
+ * <tr><td>like      </td><td>like                             </td><td>firstName=like:John                      </td></tr>
+ * <tr><td>set       </td><td>value exists (not null)          </td><td>firstName=set:                           </td></tr>
+ * <tr><td>notset    </td><td>value is not set (null)          </td><td>firstName=notset:                        </td></tr>
  * </table>
  *
  * <p>References:
@@ -28,11 +33,16 @@ package de.digitalcollections.model.api.filter.enums;
  */
 public enum FilterOperation {
   EQUALS("eq", OperandCount.SINGLEVALUE),
+  EQUALS_OR_NOT_SET("eq_notset", OperandCount.SINGLEVALUE),
   NOT_EQUALS("neq", OperandCount.SINGLEVALUE),
   GREATER_THAN("gt", OperandCount.SINGLEVALUE),
+  GREATER_THAN_OR_NOT_SET("gt_notset", OperandCount.SINGLEVALUE),
   GREATER_THAN_OR_EQUAL_TO("gte", OperandCount.SINGLEVALUE),
+  GREATER_THAN_OR_EQUAL_TO_OR_NOT_SET("gte_notset", OperandCount.SINGLEVALUE),
   LESS_THAN("lt", OperandCount.SINGLEVALUE),
-  LESSTHAN_OR_EQUAL_TO("lte", OperandCount.SINGLEVALUE),
+  LESS_THAN_OR_NOT_SET("lt_notset", OperandCount.SINGLEVALUE),
+  LESS_THAN_OR_EQUAL_TO("lte", OperandCount.SINGLEVALUE),
+  LESS_THAN_OR_EQUAL_TO_OR_NOT_SET("lte_notset", OperandCount.SINGLEVALUE),
   IN("in", OperandCount.MULTIVALUE),
   NOT_IN("nin", OperandCount.MULTIVALUE),
   BETWEEN("btn", OperandCount.MIN_MAX_VALUES),
