719acfa0020634405ef2fd82541500b8f5aac3aa
Ralf Eichinger <ralf.eichinger@bsb-muenchen.de>
2020-11-07 14:53:09 +0100

    Add date and time published and creators

--- dc-model/src/main/java/de/digitalcollections/model/api/identifiable/entity/Article.java
+++ dc-model/src/main/java/de/digitalcollections/model/api/identifiable/entity/Article.java
@@ -1,6 +1,10 @@
 package de.digitalcollections.model.api.identifiable.entity;
 
+import de.digitalcollections.model.api.identifiable.entity.agent.Agent;
 import de.digitalcollections.model.api.identifiable.parts.structuredcontent.LocalizedStructuredContent;
+import java.time.LocalDate;
+import java.util.List;
+import org.wikidata.wdtk.datamodel.interfaces.TimeValue;
 
 /**
  * Article is used to manage cultural articles and their hierarchy.
@@ -9,7 +13,19 @@ import de.digitalcollections.model.api.identifiable.parts.structuredcontent.Loca
  */
 public interface Article<A extends Article> extends Entity {
 
+  List<Agent> getCreators();
+
+  void setCreators(List<Agent> creators);
+
+  LocalDate getDatePublished();
+
+  void setDatePublished(LocalDate datePublished);
+
   LocalizedStructuredContent getText();
 
   void setText(LocalizedStructuredContent text);
+
+  TimeValue getTimeValuePublished();
+
+  void setTimeValuePublished(TimeValue timeValuePublished);
 }
--- dc-model/src/main/java/de/digitalcollections/model/impl/identifiable/entity/ArticleImpl.java
+++ dc-model/src/main/java/de/digitalcollections/model/impl/identifiable/entity/ArticleImpl.java
@@ -1,16 +1,34 @@
 package de.digitalcollections.model.impl.identifiable.entity;
 
 import de.digitalcollections.model.api.identifiable.entity.Article;
+import de.digitalcollections.model.api.identifiable.entity.agent.Agent;
 import de.digitalcollections.model.api.identifiable.entity.enums.EntityType;
 import de.digitalcollections.model.api.identifiable.parts.structuredcontent.LocalizedStructuredContent;
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+import org.wikidata.wdtk.datamodel.interfaces.TimeValue;
 
 public class ArticleImpl extends EntityImpl implements Article<Article> {
 
+  private List<Agent> creators = new ArrayList<>();
+  private LocalDate datePublished;
   private LocalizedStructuredContent text;
+  private TimeValue timeValuePublished;
 
   public ArticleImpl() {
     super();
     this.entityType = EntityType.ARTICLE;
+  }
+
+  @Override
+  public List<Agent> getCreators() {
+    return creators;
+  }
+
+  @Override
+  public LocalDate getDatePublished() {
+    return datePublished;
   }
 
   @Override
@@ -19,7 +37,27 @@ public class ArticleImpl extends EntityImpl implements Article<Article> {
   }
 
   @Override
+  public TimeValue getTimeValuePublished() {
+    return timeValuePublished;
+  }
+
+  @Override
+  public void setCreators(List<Agent> creators) {
+    this.creators = creators;
+  }
+
+  @Override
+  public void setDatePublished(LocalDate datePublished) {
+    this.datePublished = datePublished;
+  }
+
+  @Override
   public void setText(LocalizedStructuredContent text) {
     this.text = text;
+  }
+
+  @Override
+  public void setTimeValuePublished(TimeValue timeValuePublished) {
+    this.timeValuePublished = timeValuePublished;
   }
 }
