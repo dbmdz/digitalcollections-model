2067dc127e1de15eec3f3b7ab0e5fbb2654e942e
Ralf Eichinger <ralf.eichinger@bsb-muenchen.de>
2021-02-03 11:08:20 +0100

    Remove duplicate inheritance of entity

--- dc-model/src/main/java/de/digitalcollections/model/api/identifiable/entity/agent/Family.java
+++ dc-model/src/main/java/de/digitalcollections/model/api/identifiable/entity/agent/Family.java
@@ -1,5 +1,3 @@
 package de.digitalcollections.model.api.identifiable.entity.agent;
 
-import de.digitalcollections.model.api.identifiable.entity.Entity;
-
-public interface Family extends Entity, Agent {}
+public interface Family extends Agent {}
