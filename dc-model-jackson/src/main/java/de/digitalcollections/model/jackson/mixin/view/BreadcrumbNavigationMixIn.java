0330f8fcd31fd1a3c89de3650a9601afc1a94c3b
Ralf Eichinger <ralf.eichinger@bsb-muenchen.de>
2021-02-19 12:52:27 +0100

    New breadcrumb navigation model

--- dc-model-jackson/src/main/java/de/digitalcollections/model/jackson/mixin/view/BreadcrumbNavigationMixIn.java
+++ dc-model-jackson/src/main/java/de/digitalcollections/model/jackson/mixin/view/BreadcrumbNavigationMixIn.java
@@ -2,7 +2,7 @@ package de.digitalcollections.model.jackson.mixin.view;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
-import de.digitalcollections.model.identifiable.web.BreadcrumbNavigation;
+import de.digitalcollections.model.view.BreadcrumbNavigation;
 
 @JsonDeserialize(as = BreadcrumbNavigation.class)
 @JsonIgnoreProperties(ignoreUnknown = true)
