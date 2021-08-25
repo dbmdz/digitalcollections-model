958347849a16440eaaa33c602f58186927cc3bc0
David Förster <david.foerster@bsb-muenchen.de>
2021-08-25 11:56:21 +0200

    Add `equals` and other handy methods to `LocalizedUrlAliases`

--- dc-model/src/main/java/de/digitalcollections/model/alias/LocalizedUrlAliases.java
+++ dc-model/src/main/java/de/digitalcollections/model/alias/LocalizedUrlAliases.java
@@ -4,19 +4,28 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Locale;
+import java.util.stream.Collectors;
 
 public class LocalizedUrlAliases extends HashMap<Locale, List<UrlAlias>> {
 
-  public LocalizedUrlAliases() {
-    super();
-  }
-
+  /**
+   * Constructor, can be called without parameters as well, i.e. {@code new LocalizedUrlAliases()}
+   *
+   * @param urlAliases Not {@code null}
+   */
   public LocalizedUrlAliases(UrlAlias... urlAliases) {
-    this();
+    super();
     this.add(urlAliases);
   }
 
+  public LocalizedUrlAliases(List<UrlAlias> urlAliases) {
+    this(urlAliases.toArray(UrlAlias[]::new));
+  }
+
   public void add(UrlAlias... urlAliases) {
+    if (urlAliases == null) {
+      return;
+    }
     for (UrlAlias urlAlias : urlAliases) {
       this.compute(
           urlAlias.getTargetLanguage(),
@@ -28,6 +37,32 @@ public class LocalizedUrlAliases extends HashMap<Locale, List<UrlAlias>> {
             return listOfAliases;
           });
     }
+  }
+
+  /** Check whether the passed {@code UrlAlias} is contained in any of the locale specific lists. */
+  public boolean containsUrlAlias(UrlAlias urlAlias) {
+    return this.flatten().contains(urlAlias);
+  }
+
+  @Override
+  public boolean equals(Object o) {
+    if (o == this) {
+      return true;
+    }
+    if (o == null || !(o instanceof LocalizedUrlAliases)) {
+      return false;
+    }
+    LocalizedUrlAliases other = (LocalizedUrlAliases) o;
+    return super.equals(other);
+  }
+
+  /**
+   * Flatten this map to a list.
+   *
+   * @return list containing all {@code UrlAlias}es from this object
+   */
+  public List<UrlAlias> flatten() {
+    return this.values().stream().flatMap(list -> list.stream()).collect(Collectors.toList());
   }
 
   public List<Locale> getTargetLanguages() {
