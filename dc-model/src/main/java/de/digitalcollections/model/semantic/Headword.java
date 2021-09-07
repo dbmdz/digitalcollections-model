1a920917a4559429afe41331860ecbd234be529b
Ralf Eichinger <ralf.eichinger@bsb-muenchen.de>
2021-09-07 11:26:23 +0200

    Add created and last modified to headword

--- dc-model/src/main/java/de/digitalcollections/model/semantic/Headword.java
+++ dc-model/src/main/java/de/digitalcollections/model/semantic/Headword.java
@@ -1,39 +1,51 @@
 package de.digitalcollections.model.semantic;
 
+import de.digitalcollections.model.identifiable.entity.HeadwordEntry;
+import java.time.LocalDateTime;
 import java.util.Locale;
 import java.util.UUID;
 
 /**
- * See https://en.wikipedia.org/wiki/Headword and
- * https://de.wikipedia.org/wiki/Stichwort_(Dokumentation)
+ * See https://en.wikipedia.org/wiki/Headword and https://de.wikipedia.org/wiki/Stichwort_(Dokumentation)
  *
- * <p>"A headword, head word, (lemma), or sometimes catchword, is the word under which a set of
- * related dictionary or encyclopaedia entries appears. The headword is used to locate the entry,
- * and dictates its alphabetical position."
+ * <p>
+ * "A headword, head word, (lemma), or sometimes catchword, is the word under which a set of related dictionary or encyclopaedia entries appears. The headword is used to locate the entry, and dictates
+ * its alphabetical position."
  *
- * <p>(for difference to "lemma" see
- * http://www.differencebetween.net/language/difference-between-headword-and-lemma/)
+ * <p>
+ * (for difference to "lemma" see http://www.differencebetween.net/language/difference-between-headword-and-lemma/)
  *
- * <p>As it is often interchangeable, we decided for practial reasons to use this class "Headword"
- * also for a lemma.
+ * <p>
+ * As it is often interchangeable, we decided for practial reasons to use this class "Headword" also for a lemma.
  *
  * @see HeadwordEntry for the textual body to a headword/lemma.
  */
 public class Headword {
 
+  private LocalDateTime created;
   private String label;
+  private LocalDateTime lastModified;
   private Locale locale;
   private UUID uuid;
 
-  public Headword() {}
+  public Headword() {
+  }
 
   public Headword(String label, Locale locale) {
     this.label = label;
     this.locale = locale;
   }
 
+  public LocalDateTime getCreated() {
+    return created;
+  }
+
   public String getLabel() {
     return label;
+  }
+
+  public LocalDateTime getLastModified() {
+    return lastModified;
   }
 
   public Locale getLocale() {
@@ -44,8 +56,16 @@ public class Headword {
     return uuid;
   }
 
+  public void setCreated(LocalDateTime created) {
+    this.created = created;
+  }
+
   public void setLabel(String label) {
     this.label = label;
+  }
+
+  public void setLastModified(LocalDateTime lastModified) {
+    this.lastModified = lastModified;
   }
 
   public void setLocale(Locale locale) {
