743cbd90f1193c758a5a6e8a86b51e520824fe7d
Ralf Eichinger <ralf.eichinger@bsb-muenchen.de>
2021-09-17 13:09:14 +0200

    Update dc-model/src/main/java/de/digitalcollections/model/filter/FilterCriterion.java
    Co-authored-by: David Förster <david.foerster@bsb-muenchen.de>

--- dc-model/src/main/java/de/digitalcollections/model/filter/FilterCriterion.java
+++ dc-model/src/main/java/de/digitalcollections/model/filter/FilterCriterion.java
@@ -11,7 +11,7 @@ import java.util.stream.Collectors;
  *   <li>a filter operation (operator)
  *   <li>one ore more filter values to be used for filtering operation (operand(s))
  *   <li>a flag to indicate if expression has to be mapped or be used unchanged in
- *       underlying backend system (e.g. mapping Java member names to database column names; no
+ *       underlying backend system (e.g. mapping Java member names to database column names vs. no
  *       mapping needed because expression is already backend/database specific)
  * </ul>
  *
