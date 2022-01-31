4d85571d2a35268eb52d313961c16ea6c16b40d9
Ralf Eichinger <ralf.eichinger@bsb-muenchen.de>
2022-01-31 15:16:11 +0100

    Remove isascending/isdescending from json payload Remove validate() method call from set expression and add default constructor

--- dc-model-jackson/src/main/java/de/digitalcollections/model/jackson/mixin/paging/OrderMixIn.java
+++ dc-model-jackson/src/main/java/de/digitalcollections/model/jackson/mixin/paging/OrderMixIn.java
@@ -1,9 +1,17 @@
 package de.digitalcollections.model.jackson.mixin.paging;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
 import de.digitalcollections.model.paging.Order;
 
 @JsonDeserialize(as = Order.class)
 @JsonIgnoreProperties(ignoreUnknown = true)
-public abstract class OrderMixIn {}
+public abstract class OrderMixIn {
+
+  @JsonIgnore
+  public abstract boolean isAscending();
+
+  @JsonIgnore
+  public abstract boolean isDescending();
+}
