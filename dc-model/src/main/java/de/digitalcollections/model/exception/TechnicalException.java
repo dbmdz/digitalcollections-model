a5574ec846a4e53e6540e5f6a4abf07b54c06391
Ralf Eichinger <ralf.eichinger@bsb-muenchen.de>
2022-02-07 17:15:40 +0100

    Introduce TechnicalException for all technical backend problems

---
+++ dc-model/src/main/java/de/digitalcollections/model/exception/TechnicalException.java
@@ -0,0 +1,25 @@
+package de.digitalcollections.model.exception;
+
+import java.io.IOException;
+
+/**
+ * An exception indicating that there has been an error in the communication between client and
+ * server that neither was a problem on client side not on server side but between (connection,
+ * "wire", etc.)
+ */
+public class TechnicalException extends IOException {
+
+  public TechnicalException() {}
+
+  public TechnicalException(String message) {
+    super(message);
+  }
+
+  public TechnicalException(String message, Throwable cause) {
+    super(message, cause);
+  }
+
+  public TechnicalException(Throwable cause) {
+    super(cause);
+  }
+}
