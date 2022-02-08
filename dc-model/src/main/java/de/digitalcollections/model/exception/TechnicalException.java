6ea244f1b5133fc9004069e3b9f4389ca9157b3c
Ralf Eichinger <ralf.eichinger@bsb-muenchen.de>
2022-02-08 09:22:39 +0100

    Shorten javadoc to be more generic

--- dc-model/src/main/java/de/digitalcollections/model/exception/TechnicalException.java
+++ dc-model/src/main/java/de/digitalcollections/model/exception/TechnicalException.java
@@ -1,9 +1,7 @@
 package de.digitalcollections.model.exception;
 
 /**
- * An exception indicating that there has been an error in the communication between client and
- * server that neither was a problem on client side not on server side but between (connection,
- * "wire", etc.)
+ * An exception indicating that there has been a technical error.
  */
 public class TechnicalException extends Exception {
 
