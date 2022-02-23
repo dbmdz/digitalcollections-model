0a72cb96c0f3ff46fc8afa88cfeae623f39e2be9
Ralf Eichinger <ralf.eichinger@bsb-muenchen.de>
2022-02-23 14:24:31 +0100

    Add more documentation for non json-ld resources

--- dc-model/src/main/java/de/digitalcollections/model/identifiable/resource/LinkedDataFileResource.java
+++ dc-model/src/main/java/de/digitalcollections/model/identifiable/resource/LinkedDataFileResource.java
@@ -3,10 +3,13 @@ package de.digitalcollections.model.identifiable.resource;
 import java.net.URI;
 
 /**
- * This class represents a linked data file resource, referenced through an uri and specified by the
- * context definition and the corresponding (object) type. A linked data file resource can be of
- * format JSON-LD (mimetype 'application/ld+json'), e.g. an IIIF Presentation Manifest or an Entity
- * Facts person description, what is the first use case being in focus.<br>
+ * This class represents a linked data file resource, referenced through an uri. It may be specified
+ * additionally (if applicable) by the context definition and the corresponding (object) type. <br>
+ * Examples of LinkedDataFileResources are machine readable resources like Marc, MARC-XML, RDF,
+ * IIIF-Manifest, METS, JSON-LD, .... <br>
+ * A linked data file resource may be for example of format JSON-LD (mimetype
+ * 'application/ld+json'), e.g. an IIIF Presentation Manifest or an Entity Facts person description,
+ * what is the first use case being in focus.<br>
  * <br>
  * JSON-LD is designed around the concept of a "context" to provide additional mappings from JSON to
  * an RDF model.<br>
@@ -17,9 +20,11 @@ import java.net.URI;
  * the 'uri' field of FileResource to be the id.<br>
  * The 'type' specifies the type of object described in the document.<br>
  * <br>
+ * The 'context' and 'objectType' are optional if no context or objectType is known. <br>
  * Further informations:
  *
  * <ul>
+ *   <li><a href="https://en.wikipedia.org/wiki/Linked_data">Linked Data</a>
  *   <li><a href="https://en.wikipedia.org/wiki/JSON-LD">JSON-LD</a>
  *   <li><a
  *       href="https://en.wikipedia.org/wiki/Internationalized_Resource_Identifier">Internationalized
