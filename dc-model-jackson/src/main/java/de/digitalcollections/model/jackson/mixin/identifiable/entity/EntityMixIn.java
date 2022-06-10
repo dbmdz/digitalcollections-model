4738b474fb0a0e5de5d85bdec8da6ad82a1f847e
Christoph Lorenz <christoph.lorenz@bsb-muenchen.de>
2022-06-10 13:21:07 +0200

    Formatting fix

--- dc-model-jackson/src/main/java/de/digitalcollections/model/jackson/mixin/identifiable/entity/EntityMixIn.java
+++ dc-model-jackson/src/main/java/de/digitalcollections/model/jackson/mixin/identifiable/entity/EntityMixIn.java
@@ -18,26 +18,26 @@ import de.digitalcollections.model.identifiable.entity.work.Manifestation;
 import de.digitalcollections.model.identifiable.entity.work.Work;
 
 @JsonTypeInfo(
-  use = JsonTypeInfo.Id.NAME,
-  include = JsonTypeInfo.As.EXISTING_PROPERTY,
-  property = "identifiableObjectType",
-  visible = true)
+    use = JsonTypeInfo.Id.NAME,
+    include = JsonTypeInfo.As.EXISTING_PROPERTY,
+    property = "identifiableObjectType",
+    visible = true)
 @JsonIgnoreProperties(ignoreUnknown = true)
 @JsonSubTypes({
-    // need to be uppercase (and included as EXISTING_PROPERTY) to reuse enum field values (see
-    // IdentifiableObjectType) for deserializing:
-    @JsonSubTypes.Type(value = Article.class, name = "ARTICLE"),
-    @JsonSubTypes.Type(value = Collection.class, name = "COLLECTION"),
-    @JsonSubTypes.Type(value = DigitalObject.class, name = "DIGITAL_OBJECT"),
-    @JsonSubTypes.Type(value = Entity.class, name = "ENTITY"),
-    @JsonSubTypes.Type(value = Expression.class, name = "EXPRESSION"),
-    @JsonSubTypes.Type(value = GeoLocation.class, name = "GEOLOCATION"),
-    @JsonSubTypes.Type(value = HeadwordEntry.class, name = "HEADWORD_ENTRY"),
-    @JsonSubTypes.Type(value = Item.class, name = "ITEM"),
-    @JsonSubTypes.Type(value = Manifestation.class, name = "MANIFESTATION"),
-    @JsonSubTypes.Type(value = Project.class, name = "PROJECT"),
-    @JsonSubTypes.Type(value = Topic.class, name = "TOPIC"),
-    @JsonSubTypes.Type(value = Website.class, name = "WEBSITE"),
-    @JsonSubTypes.Type(value = Work.class, name = "WORK")
+  // need to be uppercase (and included as EXISTING_PROPERTY) to reuse enum field values (see
+  // IdentifiableObjectType) for deserializing:
+  @JsonSubTypes.Type(value = Article.class, name = "ARTICLE"),
+  @JsonSubTypes.Type(value = Collection.class, name = "COLLECTION"),
+  @JsonSubTypes.Type(value = DigitalObject.class, name = "DIGITAL_OBJECT"),
+  @JsonSubTypes.Type(value = Entity.class, name = "ENTITY"),
+  @JsonSubTypes.Type(value = Expression.class, name = "EXPRESSION"),
+  @JsonSubTypes.Type(value = GeoLocation.class, name = "GEOLOCATION"),
+  @JsonSubTypes.Type(value = HeadwordEntry.class, name = "HEADWORD_ENTRY"),
+  @JsonSubTypes.Type(value = Item.class, name = "ITEM"),
+  @JsonSubTypes.Type(value = Manifestation.class, name = "MANIFESTATION"),
+  @JsonSubTypes.Type(value = Project.class, name = "PROJECT"),
+  @JsonSubTypes.Type(value = Topic.class, name = "TOPIC"),
+  @JsonSubTypes.Type(value = Website.class, name = "WEBSITE"),
+  @JsonSubTypes.Type(value = Work.class, name = "WORK")
 })
 public interface EntityMixIn {}
