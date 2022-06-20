ccb63167fea423daedbdc9bec2f972cdce49e48c
Matthias Lindinger <matthias.lindinger@bsb-muenchen.de>
2022-06-20 12:09:13 +0200

    Fix code style

--- dc-model/src/test/java/de/digitalcollections/model/identifiable/alias/UrlAliasTest.java
+++ dc-model/src/test/java/de/digitalcollections/model/identifiable/alias/UrlAliasTest.java
@@ -14,66 +14,55 @@ class UrlAliasTest {
   @Test
   @DisplayName("fills the field targetEntityType for a collection")
   void fillTargetEntityTypeForColletion() {
-    UrlAlias urlAlias = UrlAlias.builder()
-        .targetType(
-            IdentifiableObjectType.COLLECTION,
-            IdentifiableType.ENTITY,
-            EntityType.COLLECTION
-        )
-        .build();
+    UrlAlias urlAlias =
+        UrlAlias.builder()
+            .targetType(
+                IdentifiableObjectType.COLLECTION, IdentifiableType.ENTITY, EntityType.COLLECTION)
+            .build();
     assertThat(urlAlias.getTargetEntityType()).isEqualTo(EntityType.COLLECTION);
   }
 
   @Test
   @DisplayName("fills the field targetEntityType with null for a webpage")
   void fillTargetEntityTypeForWebpage() {
-    UrlAlias urlAlias = UrlAlias.builder()
-        .targetType(
-            IdentifiableObjectType.WEBPAGE,
-            IdentifiableType.RESOURCE,
-            null
-        )
-        .build();
+    UrlAlias urlAlias =
+        UrlAlias.builder()
+            .targetType(IdentifiableObjectType.WEBPAGE, IdentifiableType.RESOURCE, null)
+            .build();
     assertThat(urlAlias.getTargetEntityType()).isNull();
   }
 
   @Test
   @DisplayName("fills the field targetEntityType for a family")
   void fillTargetEntityTypeForFamily() {
-    UrlAlias urlAlias = UrlAlias.builder()
-        .targetType(
-            IdentifiableObjectType.FAMILY,
-            IdentifiableType.ENTITY,
-            EntityType.FAMILY
-        )
-        .build();
+    UrlAlias urlAlias =
+        UrlAlias.builder()
+            .targetType(IdentifiableObjectType.FAMILY, IdentifiableType.ENTITY, EntityType.FAMILY)
+            .build();
     assertThat(urlAlias.getTargetEntityType()).isEqualTo(EntityType.FAMILY);
   }
 
   @Test
   @DisplayName("fills the field targetEntityType with null for an ApplicationFileResource")
   void fillTargetEntityTypeForFileResource() {
-    UrlAlias urlAlias = UrlAlias.builder()
-        .targetType(
-            IdentifiableObjectType.APPLICATION_FILE_RESOURCE,
-            IdentifiableType.RESOURCE,
-            null
-        )
-        .build();
+    UrlAlias urlAlias =
+        UrlAlias.builder()
+            .targetType(
+                IdentifiableObjectType.APPLICATION_FILE_RESOURCE, IdentifiableType.RESOURCE, null)
+            .build();
     assertThat(urlAlias.getTargetEntityType()).isNull();
   }
 
   @Test
   @DisplayName("fills the field targetEntityType with geolocation for a human settlement")
   void fillTargetEntityTypeForHumanSettlement() {
-    UrlAlias urlAlias = UrlAlias.builder()
-        .targetType(
-            IdentifiableObjectType.HUMAN_SETTLEMENT,
-            IdentifiableType.ENTITY,
-            EntityType.GEOLOCATION
-        )
-        .build();
+    UrlAlias urlAlias =
+        UrlAlias.builder()
+            .targetType(
+                IdentifiableObjectType.HUMAN_SETTLEMENT,
+                IdentifiableType.ENTITY,
+                EntityType.GEOLOCATION)
+            .build();
     assertThat(urlAlias.getTargetEntityType()).isEqualTo(EntityType.GEOLOCATION);
   }
-
 }
