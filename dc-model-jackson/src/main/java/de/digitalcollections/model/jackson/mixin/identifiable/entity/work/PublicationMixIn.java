144bdbcd67b4aedbbb6ad19e344ae30d2060ed31
Christoph Lorenz <christoph.lorenz@bsb-muenchen.de>
2022-09-15 16:03:54 +0200

    Publication is no longer an own standing object

--- dc-model-jackson/src/main/java/de/digitalcollections/model/jackson/mixin/identifiable/entity/work/PublicationMixIn.java
+++ dc-model-jackson/src/main/java/de/digitalcollections/model/jackson/mixin/identifiable/entity/work/PublicationMixIn.java
@@ -1,12 +1,9 @@
 package de.digitalcollections.model.jackson.mixin.identifiable.entity.work;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
-import com.fasterxml.jackson.annotation.JsonTypeName;
 import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
 import de.digitalcollections.model.identifiable.entity.work.Publication;
-import de.digitalcollections.model.jackson.mixin.UniqueObjectMixIn;
 
 @JsonDeserialize(as = Publication.class)
 @JsonIgnoreProperties(ignoreUnknown = true)
-@JsonTypeName("PUBLICATION")
-public interface PublicationMixIn extends UniqueObjectMixIn {}
+public interface PublicationMixIn {}
