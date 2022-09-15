089090a6daeb8bd66c021b4effd9766d6e56ead2
Christoph Lorenz <christoph.lorenz@bsb-muenchen.de>
2022-09-15 16:46:35 +0200

    Add equals and hashCode to Publication

--- dc-model/src/main/java/de/digitalcollections/model/identifiable/entity/work/Publication.java
+++ dc-model/src/main/java/de/digitalcollections/model/identifiable/entity/work/Publication.java
@@ -3,6 +3,7 @@ package de.digitalcollections.model.identifiable.entity.work;
 import de.digitalcollections.model.identifiable.entity.agent.Agent;
 import de.digitalcollections.model.identifiable.entity.geo.location.HumanSettlement;
 import java.util.List;
+import java.util.Objects;
 import lombok.experimental.SuperBuilder;
 
 /** Example: Mayence, Anvers, Bruxelles : chez les fils de B. Schott */
@@ -23,6 +24,25 @@ public class Publication {
     this.publicationLocations = publicationLocations;
     this.publishers = publishers;
     this.publishersPresentation = publishersPresentation;
+  }
+
+  @Override
+  public boolean equals(Object o) {
+    if (this == o) {
+      return true;
+    }
+    if (!(o instanceof Publication)) {
+      return false;
+    }
+    Publication that = (Publication) o;
+    return Objects.equals(publicationLocations, that.publicationLocations)
+        && Objects.equals(publishers, that.publishers)
+        && Objects.equals(publishersPresentation, that.publishersPresentation);
+  }
+
+  @Override
+  public int hashCode() {
+    return Objects.hash(publicationLocations, publishers, publishersPresentation);
   }
 
   public List<HumanSettlement> getPublicationLocations() {
