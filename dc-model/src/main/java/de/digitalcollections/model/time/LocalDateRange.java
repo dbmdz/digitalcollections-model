f52b2fefd1b63fb20c7c53eee66ed9cb24924581
Christoph Lorenz <christoph.lorenz@bsb-muenchen.de>
2022-10-26 15:34:12 +0200

    Add missing equals/hashCode to LocalDateRange

--- dc-model/src/main/java/de/digitalcollections/model/time/LocalDateRange.java
+++ dc-model/src/main/java/de/digitalcollections/model/time/LocalDateRange.java
@@ -1,6 +1,7 @@
 package de.digitalcollections.model.time;
 
 import java.time.LocalDate;
+import java.util.Objects;
 
 public class LocalDateRange {
 
@@ -28,5 +29,27 @@ public class LocalDateRange {
 
   public void setStart(LocalDate start) {
     this.start = start;
+  }
+
+  @Override
+  public boolean equals(Object o) {
+    if (this == o) {
+      return true;
+    }
+    if (!(o instanceof LocalDateRange)) {
+      return false;
+    }
+    LocalDateRange that = (LocalDateRange) o;
+    return Objects.equals(end, that.end) && Objects.equals(start, that.start);
+  }
+
+  @Override
+  public int hashCode() {
+    return Objects.hash(end, start);
+  }
+
+  @Override
+  public String toString() {
+    return "LocalDateRange{" + "end=" + end + ", start=" + start + '}';
   }
 }
