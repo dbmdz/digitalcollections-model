d07a965b5e40d71aabbdc9d17d90b92ac2363beb
Christoph Lorenz <christoph.lorenz@bsb-muenchen.de>
2022-11-02 10:13:42 +0100

    Review

--- dc-model-jackson/src/main/java/de/digitalcollections/model/jackson/mixin/identifiable/entity/work/PublisherMixIn.java
+++ dc-model-jackson/src/main/java/de/digitalcollections/model/jackson/mixin/identifiable/entity/work/PublisherMixIn.java
@@ -1,9 +1,11 @@
 package de.digitalcollections.model.jackson.mixin.identifiable.entity.work;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonTypeName;
 import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
 import de.digitalcollections.model.identifiable.entity.work.Publisher;
 
 @JsonDeserialize(as = Publisher.class)
 @JsonIgnoreProperties(ignoreUnknown = true)
+@JsonTypeName("PUBLISHER")
 public interface PublisherMixIn {}
