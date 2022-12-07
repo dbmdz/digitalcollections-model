517e4f05ebbc93909a81c7edee759662b6a6f2c6
Christoph Lorenz <christoph.lorenz@bsb-muenchen.de>
2022-12-07 16:00:13 +0100

    Add missing builders

--- dc-model/src/main/java/de/digitalcollections/model/identifiable/agent/FamilyName.java
+++ dc-model/src/main/java/de/digitalcollections/model/identifiable/agent/FamilyName.java
@@ -5,11 +5,13 @@ import de.digitalcollections.model.identifiable.IdentifiableType;
 import de.digitalcollections.model.identifiable.Identifier;
 import de.digitalcollections.model.text.LocalizedText;
 import java.util.Set;
+import lombok.experimental.SuperBuilder;
 
 /**
  * see Wikidata https://www.wikidata.org/wiki/Q101352: "Part of a naming scheme for individuals,
  * used in many cultures worldwide."
  */
+@SuperBuilder(buildMethodName = "prebuild")
 public class FamilyName extends Identifiable {
 
   public FamilyName() {
@@ -27,5 +29,48 @@ public class FamilyName extends Identifiable {
   protected void init() {
     super.init();
     this.type = IdentifiableType.RESOURCE;
+  }
+
+  @Override
+  public String toString() {
+    return "FamilyName{"
+        + "description="
+        + description
+        + ", identifiableObjectType="
+        + identifiableObjectType
+        + ", identifiers="
+        + identifiers
+        + ", label="
+        + label
+        + ", localizedUrlAliases="
+        + localizedUrlAliases
+        + ", previewImage="
+        + previewImage
+        + ", previewImageRenderingHints="
+        + previewImageRenderingHints
+        + ", tags="
+        + tags
+        + ", type="
+        + type
+        + ", created="
+        + created
+        + ", lastModified="
+        + lastModified
+        + ", uuid="
+        + uuid
+        + '}';
+  }
+
+  public abstract static class FamilyNameBuilder<
+          C extends FamilyName, B extends FamilyNameBuilder<C, B>>
+      extends IdentifiableBuilder<C, B> {
+
+    @Override
+    public C build() {
+      C c = prebuild();
+      c.init();
+      setInternalReferences(c);
+      return c;
+    }
   }
 }
--- dc-model/src/main/java/de/digitalcollections/model/identifiable/agent/GivenName.java
+++ dc-model/src/main/java/de/digitalcollections/model/identifiable/agent/GivenName.java
@@ -4,12 +4,15 @@ import de.digitalcollections.model.identifiable.Identifiable;
 import de.digitalcollections.model.identifiable.IdentifiableType;
 import de.digitalcollections.model.identifiable.Identifier;
 import de.digitalcollections.model.text.LocalizedText;
+import java.util.Objects;
 import java.util.Set;
+import lombok.experimental.SuperBuilder;
 
 /**
  * see WikiData https://www.wikidata.org/wiki/Q202444: "Name typically used to differentiate people
  * from the same family, clan, or other social group who have a common last name."
  */
+@SuperBuilder(buildMethodName = "prebuild")
 public class GivenName extends Identifiable {
 
   private Gender gender;
@@ -44,5 +47,70 @@ public class GivenName extends Identifiable {
     MALE, // see https://www.wikidata.org/wiki/Q12308941
     FEMALE, // see https://www.wikidata.org/wiki/Q11879590
     UNISEX; // see https://www.wikidata.org/wiki/Q3409032
+  }
+
+  @Override
+  public String toString() {
+    return "GivenName{"
+        + "gender="
+        + gender
+        + ", description="
+        + description
+        + ", identifiableObjectType="
+        + identifiableObjectType
+        + ", identifiers="
+        + identifiers
+        + ", label="
+        + label
+        + ", localizedUrlAliases="
+        + localizedUrlAliases
+        + ", previewImage="
+        + previewImage
+        + ", previewImageRenderingHints="
+        + previewImageRenderingHints
+        + ", tags="
+        + tags
+        + ", type="
+        + type
+        + ", created="
+        + created
+        + ", lastModified="
+        + lastModified
+        + ", uuid="
+        + uuid
+        + '}';
+  }
+
+  @Override
+  public boolean equals(Object o) {
+    if (this == o) {
+      return true;
+    }
+    if (!(o instanceof GivenName)) {
+      return false;
+    }
+    if (!super.equals(o)) {
+      return false;
+    }
+    GivenName givenName = (GivenName) o;
+    return gender == givenName.gender;
+  }
+
+  @Override
+  public int hashCode() {
+    return Objects.hash(super.hashCode(), gender);
+  }
+
+  public abstract static class GivenNameBuilder<
+          C extends GivenName, B extends GivenNameBuilder<C, B>>
+      extends IdentifiableBuilder<C, B> {
+
+    @Override
+    public C build() {
+      C c = prebuild();
+      c.init();
+      setInternalReferences(c);
+      return c;
+    }
   }
 }
