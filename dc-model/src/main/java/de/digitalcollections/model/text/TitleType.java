8f3b4f2ea5c958879defa596283e9c19a3990a48
Christoph Lorenz <christoph.lorenz@bsb-muenchen.de>
2023-03-23 15:07:41 +0100

    Review

--- dc-model/src/main/java/de/digitalcollections/model/text/TitleType.java
+++ dc-model/src/main/java/de/digitalcollections/model/text/TitleType.java
@@ -11,14 +11,10 @@ public class TitleType extends MainSubType {
   }
 
   public TitleType(String mainType, String subType) {
-    super(mainType, (subType == null || subType.isEmpty() || subType.isBlank()) ? null : subType);
+    super(mainType, (subType == null || subType.isBlank()) ? null : subType);
   }
 
   public void setSubType(String subType) {
-    if (subType == null || subType.isEmpty() || subType.isBlank()) {
-      super.setSubType(null);
-    } else {
-      super.setSubType(subType);
-    }
+    super.setSubType((subType == null || subType.isBlank()) ? null : subType);
   }
 }
