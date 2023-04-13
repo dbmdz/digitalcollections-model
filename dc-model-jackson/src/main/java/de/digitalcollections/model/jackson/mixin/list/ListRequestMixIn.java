638bd7d25ade5889552c7f7bdb98e13809aff7f8
David Förster <david.foerster@bsb-muenchen.de>
2023-04-13 16:54:32 +0200

    Fix jackson serialization
    Remove subtypes of:
    - `UniqueObject`
    - `ListRequest`

--- dc-model-jackson/src/main/java/de/digitalcollections/model/jackson/mixin/list/ListRequestMixIn.java
+++ dc-model-jackson/src/main/java/de/digitalcollections/model/jackson/mixin/list/ListRequestMixIn.java
@@ -1,15 +1,12 @@
 package de.digitalcollections.model.jackson.mixin.list;
 
-import com.fasterxml.jackson.annotation.JsonSubTypes;
-import com.fasterxml.jackson.annotation.JsonTypeInfo;
 import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
 import de.digitalcollections.model.list.ListRequest;
-import de.digitalcollections.model.list.paging.PageRequest;
 
 @JsonDeserialize(as = ListRequest.class)
-@JsonTypeInfo(use = JsonTypeInfo.Id.NAME, property = "listRequestType", visible = true)
-@JsonSubTypes({
-  @JsonSubTypes.Type(value = ListRequest.class, name = "LIST_REQUEST"),
-  @JsonSubTypes.Type(value = PageRequest.class, name = "PAGE_REQUEST")
-})
+// @JsonTypeInfo(use = JsonTypeInfo.Id.NAME, property = "listRequestType", visible = true)
+// @JsonSubTypes({
+//  @JsonSubTypes.Type(value = ListRequest.class, name = "LIST_REQUEST"),
+//  @JsonSubTypes.Type(value = PageRequest.class, name = "PAGE_REQUEST")
+// })
 public abstract class ListRequestMixIn {}
