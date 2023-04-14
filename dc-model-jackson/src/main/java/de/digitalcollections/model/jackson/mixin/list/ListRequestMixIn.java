7c505e80b8e9f02cf12d589936083551504f8e4d
David Förster <david.foerster@bsb-muenchen.de>
2023-04-14 10:55:27 +0200

    Update dc-model-jackson/src/main/java/de/digitalcollections/model/jackson/mixin/list/ListRequestMixIn.java

--- dc-model-jackson/src/main/java/de/digitalcollections/model/jackson/mixin/list/ListRequestMixIn.java
+++ dc-model-jackson/src/main/java/de/digitalcollections/model/jackson/mixin/list/ListRequestMixIn.java
@@ -4,9 +4,4 @@ import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
 import de.digitalcollections.model.list.ListRequest;
 
 @JsonDeserialize(as = ListRequest.class)
-// @JsonTypeInfo(use = JsonTypeInfo.Id.NAME, property = "listRequestType", visible = true)
-// @JsonSubTypes({
-//  @JsonSubTypes.Type(value = ListRequest.class, name = "LIST_REQUEST"),
-//  @JsonSubTypes.Type(value = PageRequest.class, name = "PAGE_REQUEST")
-// })
 public abstract class ListRequestMixIn {}
