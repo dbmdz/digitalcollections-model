d178f31aadbb78f0f04572dd5a46bf3816ead920
David Förster <david.foerster@bsb-muenchen.de>
2023-04-14 10:55:09 +0200

    Update dc-model-jackson/src/main/java/de/digitalcollections/model/jackson/mixin/UniqueObjectMixIn.java

--- dc-model-jackson/src/main/java/de/digitalcollections/model/jackson/mixin/UniqueObjectMixIn.java
+++ dc-model-jackson/src/main/java/de/digitalcollections/model/jackson/mixin/UniqueObjectMixIn.java
@@ -3,16 +3,5 @@ package de.digitalcollections.model.jackson.mixin;
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonInclude.Include;
 
-// @JsonTypeInfo(use = JsonTypeInfo.Id.NAME, property = "objectType", visible = true)
-// @JsonSubTypes({
-//  @JsonSubTypes.Type(value = Headword.class, name = "HEADWORD"),
-//  @JsonSubTypes.Type(value = Identifiable.class, name = "IDENTIFIABLE"),
-//  @JsonSubTypes.Type(value = Identifier.class, name = "IDENTIFIER"),
-//  @JsonSubTypes.Type(value = IdentifierType.class, name = "IDENTIFIER_TYPE"),
-//  @JsonSubTypes.Type(value = License.class, name = "LICENSE"),
-//  @JsonSubTypes.Type(value = Predicate.class, name = "PREDICATE"),
-//  @JsonSubTypes.Type(value = RenderingTemplate.class, name = "RENDERING_TEMPLATE"),
-//  @JsonSubTypes.Type(value = User.class, name = "USER")
-// })
 @JsonInclude(value = Include.NON_EMPTY)
 public interface UniqueObjectMixIn {}
